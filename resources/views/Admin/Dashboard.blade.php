ffe86b98716d93fafa74199009a7788267e5ca3c
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-06 18:46:04 +0100

    Update Dashboard.blade.php

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -22,7 +22,11 @@
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
-                       
+                       <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Earnings (Daily)</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{\App\Models\Facture::get()->sum('montant_total')}} DH's</div>
+                        </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
                         </div>
