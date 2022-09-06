a7f09c32851d2b0446db5ed9c1edb71443283e0c
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-06 18:10:09 +0100

    Update VilleSeeder.php

--- database/seeders/VilleSeeder.php
+++ database/seeders/VilleSeeder.php
@@ -15,7 +15,7 @@ class VilleSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Ville')->create(
+        DB::table('ville')->create(
                 ['id'=>'1','ville' =>'Aïn Harrouda','region' => '1'],
                 ['id'=>'2','ville' =>'Ben Yakhlef','region' => '1'],
                 ['id'=>'3','ville' =>'Bouskoura','region' => '1'],
