55b0553509bf5cef401e0e269cb6140e4271366c
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-06 19:00:49 +0100

    Update AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -17,7 +17,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth()->check() &&  Auth::user()->user_type == 'admin') {
+        if (Auth()->check()) {
             return $next($request);
         } else {
             return redirect('home');
