ffc691707c24cd6bbea2c83c65362586bbda9b4b
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-06 18:57:25 +0100

    Update Dashboard.blade.php

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -24,7 +24,7 @@
                     <div class="row no-gutters align-items-center">
                        <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Daily)</div>
+                                Earnings (Total)</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{\App\Models\Facture::get()->sum('montant_total')}} DH's</div>
                         </div>
                         <div class="col-auto">
