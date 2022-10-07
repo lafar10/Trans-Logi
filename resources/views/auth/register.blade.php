2b86c0f6d11b79dc2d03f2f345cc719f2262d41b
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-10-07 22:12:05 +0100

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,11 +54,17 @@
                         </ul>
                         <br>
                         <br>
-                         @if(Auth::user())
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/Dashboard">Dashboard</a>
-                        </li>
-                    @endif
+                         <div class="container">
+                            @auth
+
+                        @endauth
+
+                        @guest
+                            <div class="alert alert-primary">
+                                créer un nouveau compte pour accéder au tableau de bord
+                          </div>
+                        @endguest
+                       </div>
                         <!--/ End Tab Nav -->
                     </div>
                     <div class="tab-content" id="myTabContent">
